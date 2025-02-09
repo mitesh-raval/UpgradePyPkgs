cd45c9910f67b8fc9363da29072d9f7740f337f7
Mitesh Raval <mitesh22love@gmail.com>
2025-02-09 18:08:43 +1100

    formatting updates

--- pkgNames.py
+++ pkgNames.py
@@ -2,22 +2,26 @@
 
 import argparse
 
-def main() :
-    parser = argparse.ArgumentParser(description='Create file with package NAMES \
-        ONLY list from pip3 list -o output')
-    parser.add_argument('src', help='(Relative) Path to source file e.g. pkgs.txt')
-    parser.add_argument('dest', help='(Relative) New location/file name e.g. upgrade.txt')
+
+def main():
+    parser = argparse.ArgumentParser(
+        description="Create file with package NAMES \
+        ONLY list from pip3 list -o output"
+    )
+    parser.add_argument("src", help="(Relative) Path to source file e.g. pkgs.txt")
+    parser.add_argument("dest", help="(Relative) New location/file name e.g. upgrade.txt")
     args = parser.parse_args()
-    with open(args.src, 'r', newline='\n') as f:
+    with open(args.src, "r", newline="\n") as f:
         content = f.read()
 
-    linesList = list(map(str.strip,iter(content.splitlines())))
+    linesList = list(map(str.strip, iter(content.splitlines())))
     linesList = linesList[2:]
-    namesList = [l.split(' ')[0] for l in linesList]
+    namesList = [ln.split(" ")[0] for ln in linesList]
 
-    with open(args.dest, 'w', newline='\n') as f:
+    with open(args.dest, "w", newline="\n") as f:
         for n in namesList:
-            f.write(n + '\n')
+            f.write(n + "\n")
         f.flush()
+
 
 main()
